5b6ed44a92622625d623b660d121f3d8851c1cd5
Karel Bemelmans <Karel.Bemelmans@UNIBET.com>
2016-10-24 15:32:04 +0200

    Updated example with build script container linking

--- build.sh
+++ build.sh
@@ -6,17 +6,18 @@
 # - start this container wit the local work directory mounted
 
 # This needs to be something totally random
-RANDOM_CONTAINERNAME=${GO_REVISION:-namespace/tmpcontainer}
+RANDOM_CONTAINER_NAME=${GO_REVISION:-namespace/tmpcontainer}
+SCRIPTS_CONTAINER=$AWS_ECR_LOCATION/unibet/pipeline-go-scripts:latest
 
 # 1. Create image
-docker build -t $RANDOM_CONTAINERNAME -f Dockerfile-build .
+docker build -t $RANDOM_CONTAINER_NAME -f Dockerfile-build .
 
-# 2. Run container
+# 2. Create a container for the scripts image
+docker create -v /opt --name pipeline-go-scripts-container SCRIPTS_CONTAINER
+
+# 2. Run container, link it with the scripts container
 docker run --rm \
   -v $(pwd)/src:/src \
   -v $(pwd)/output:/output \
-  $RANDOM_CONTAINERNAME
-
-echo "Output:"
-ls -al output/
-cat output/*
+  -v-volumes-from pipeline-go-scripts-container \
+  $RANDOM_CONTAINER_NAME
--- docker-build/run.sh
+++ docker-build/run.sh
@@ -5,5 +5,10 @@
 # Clean the output directory
 rm -rf /output/*
 
+echo "Output of /opt:"
+ls -al /opt
+echo ""
+echo ""
+
 # Generate a file in the output directory
 echo "This is a successful build!" >> /output/output.txt
